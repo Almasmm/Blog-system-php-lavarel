e9352ff4f246be7632b3c06bd1353516d5d648bc
Almas <almasmurat05.kz@gmail.com>
2025-03-25 16:42:21 +0500

    Added route categories and add view for categories

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,21 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+
+        <ul class="p-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="pages/gallery.html" class="nav-link">
+                    <i class="fas fa-th-list"> </i>
+                    <p>
+                        Categories
+                    </p>
+                </a>
+            </li>
+        </ul>
+
+
+    </div>
+    <!-- /.sidebar -->
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Main\IndexController as MainIndexController;
+use App\Http\Controllers\Admin\Category\IndexController as AdminCategoryIndexController;
 use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
+use App\Http\Controllers\Main\IndexController as MainIndexController;
+use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', MainIndexController::class);
+Route::get('/', [MainIndexController::class, '__invoke']);
 
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('/', AdminIndexController::class);
+    Route::get('/', [AdminIndexController::class, '__invoke']);
+    Route::get('categories', [AdminCategoryIndexController::class, '__invoke']);
 });
 
 Auth::routes();
